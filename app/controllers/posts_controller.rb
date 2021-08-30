62af3e2cb1bf8dfc567102e83ec8b2a3928ae5c1
Hannah Malm <hannahemalm616@gmail.com>
2021-08-30 19:07:56 -0400

    commit tree 8ae6330e1f278ad38e7ded05e5f0f21db5971eca

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,31 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    #provide a list of authors to the view for the filter control
+    @authors = Author.all
+
+    #filter the @posts list based on user input - this is the logic used to get the filter in posts/index
+    #this is filtering by author
+    if !params[:author].blank?
+      #@posts = Post.where(author: params[:author])
+      #Post.by_author is a method in the model
+      @posts = Post.by_author(params[:author])
+    #this is filtering by date
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+      #@posts = Post.where("created_at >=?", Time.zone.today.beginning_of_day)
+      @posts = Post.from_today
+      else
+      #@posts = Post.where("created_at <?", Time.zone.today.beginning_of_day)
+      @posts = Post.old_news
+      end 
+    else 
+      #if no filters are applied, show all posts
+      @posts = Post.all 
+    end 
   end
+
+
 
   def show
     @post = Post.find(params[:id])
